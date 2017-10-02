85a74af8a3157cc89097b9c5cd03a28eb7da2ac4
Anton Lind <4nton1ind@gmail.com>
2017-10-02 21:18:03 +0200

    added audioswitcher packages

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
-﻿namespace GameAudioBalancer
+﻿using System;
+
+namespace GameAudioBalancer
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            
+//            Console.Write("asd");
+//            IAudioSessionManager2.
         }
     }
 }
